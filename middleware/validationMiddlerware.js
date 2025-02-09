f28b008c627de18b7f6256947ec828ba2e4de81d
Ahmed5Atiya <129235416+Ahmed5Atiya@users.noreply.github.com>
2025-02-09 08:59:30 +0200

    this is thow to validation for the get product by id

---
+++ middleware/validationMiddlerware.js
@@ -0,0 +1,8 @@
+const validationCategory = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+};
+
+module.exports = validationCategory;
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,6 +5,7 @@
 // router.get("/", getAllCategorys);
 
 // module.exports = router;
+const { body, validationResult } = require("express-validator");
 const {
   getAllCategorys,
   getASingleCategory,
@@ -16,7 +17,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", getAllCategorys);
-router.get("/:id", getASingleCategory);
+router.get(
+  "/:id",
+  body("id").isMongoId().withMessage("invalid id for the product this id"),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  },
+  getASingleCategory
+);
 router.post("/", addCategory);
 router.put("/:id", ubdateCategory);
 router.delete("/:id", deleteCategory);
