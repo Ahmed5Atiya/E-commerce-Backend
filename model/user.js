138a53dde360c59e4076290549f7575f9df21fdd
Ahmed5Atiya <129235416+Ahmed5Atiya@users.noreply.github.com>
2025-02-17 15:24:06 +0200

    this is the schema for the user and the role

--- model/user.js
+++ model/user.js
@@ -1,7 +1,40 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "the name is required"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "the email is required"],
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "the password is required"],
+      minLength: [6, "the password must be at least 6 characters"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-module.export = mongoose.model("User", userSchema);
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
